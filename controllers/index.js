d1d410dc5472a7e008ecfab2aee80d25aa14ac4c
Ivan Sedelkin <ivan.sedelkin@rocksigma.com>
2024-09-11 20:35:30 +0200

    Fixed htmx example

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
+const path = require('path');
 
+exports.Home = async function(req, res){
+  return res.render("index"); 
+}
 
-exports.Home = function(req, res){
-  console.log("here");
-  return res.send("Haram");  
+exports.Dynamic = async function(req, res){
+  // process.env.INIT_CWD is the root folder
+  return res.sendFile(path.join(process.env.INIT_CWD, 'views/test.html')); 
 }
