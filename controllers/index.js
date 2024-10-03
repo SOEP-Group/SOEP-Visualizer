51177278b2d798cab29377ba9f8e247ed7aab9c4
ArianAsghari <arianasghari123@gmail.com>
2024-10-03 11:31:40 +0200

    new changes to template

--- controllers/index.js
+++ controllers/index.js
@@ -10,6 +10,20 @@ exports.Dynamic = async function(req, res){
   return res.sendFile(path.join(process.env.INIT_CWD, 'views/test.html')); 
 }
 
+// New controller function to render the template with data
+exports.RenderSatellite = async function(req, res) {
+  // Example data to pass to the EJS template
+  const satelliteData = {
+    name: 'Hubble Space Telescope',
+    launchDate: 'April 24, 1990',
+    orbitType: 'Low Earth Orbit'
+  };
+
+  // Render the EJS template (e.g., satellite.ejs) with the data
+  res.render('satellite', satelliteData); // Ensure 'satellite.ejs' is inside the 'views' folder
+}
+
+
 
 const mockSatelliteData = {
     'satellite_1': {
--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const app = express(),
 	    bodyParser = require('body-parser'),
 	    port = 3000;
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Set the directory where your EJS files are located
+app.set('views', './views');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); // It's recommended to pass `{ extended: true }`
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/views', express.static(path.join(__dirname, 'views')));
 
+// Middleware for handling CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -19,6 +26,7 @@ app.use(function(req, res, next) {
     next();
 });
 
+// Use the routes defined in index_routes
 app.use("/", index_routes);
 
 const PORT = process.env.PORT || 3000;
