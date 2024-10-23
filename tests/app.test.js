476aed97b22a8c25b9eb293f123e32ded0d2176f
mohammed-shakir <mohammedshakir010528@gmail.com>
2024-10-23 15:00:55 +0200

    added simple CI Pipeline and tests

---
+++ tests/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /', () => {
+  it('should return 200 OK', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+  });
+});
