07ffdcbaa257df94ec0292bede84ef7957b0652a
sanlju <sanlju-9@student.ltu.se>
2024-11-21 08:31:51 +0100

    restructured some code

--- public/js/ui/predictions.js
+++ public/js/ui/predictions.js
@@ -1,6 +1,9 @@
 import { getLocation } from "./utils.js";
 
 export function initPredictions() {
+    document.getElementById("passing-prediction-header").addEventListener("click", function () {
+        toggleSection('passing-content', 'arrow-passing');
+    });
 
     document.getElementById("pass-prediction-header").addEventListener("click", function () {
         toggleSection('pass-content', 'arrow-pass');
@@ -9,11 +12,6 @@ export function initPredictions() {
     document.getElementById("re-entry-prediction-header").addEventListener("click", function () {
         toggleSection('re-entry-content', 'arrow-re-entry');
     });
-
-    document.getElementById("passing-prediction-header").addEventListener("click", function () {
-        toggleSection('passing-content', 'arrow-passing');
-    });
-
 }
 
 function toggleSection(contentId, arrowId) {
@@ -96,6 +94,20 @@ function getContent(itemId) {
     const button_styling = "btn-blue w-full";
     const location_button_styling = "bg-gray-900 text-white font-bold py-1 px-3 rounded mt-1 hover:bg-gray-700 w-full";
     switch (itemId) {
+        case "passing-content":
+            return `
+                    <div class="${input_form_styling}">
+                        <div id="toggle-section" class="flex items-center justify-between w-[250px]">
+                            <span id="toggle-text" class="mr-2 text-left flex-1">Displaying All Satellites</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="w-8 h-8 cursor-pointer ml-auto" id="toggle-svg">
+                                <path id="toggle-path" fill="#ffffff" d="M384 128c70.7 0 128 57.3 128 128s-57.3 128-128 128l-192 0c-70.7 0-128-57.3-128-128s57.3-128 128-128l192 0zM576 256c0-106-86-192-192-192L192 64C86 64 0 150 0 256S86 448 192 448l192 0c106 0 192-86 192-192zM192 352a96 96 0 1 0 0-192 96 96 0 1 0 0 192z"/>
+                            </svg>
+                        </div>
+                        <input type="text" id="location-2" placeholder="Location" class="${input_styling}">
+                        <button data-ripple="true" id="get-location-button-2" class="${location_button_styling}">Use Current Location</button>
+                        <button data-ripple="true" id="select-location-button" class="${location_button_styling}">Select Location</button>
+                    </div>
+                `;
         case "pass-content":
             return `
                 <div class="${input_form_styling}">
@@ -113,20 +125,6 @@ function getContent(itemId) {
                     <button data-ripple="true" id="calculate-re-entry-button" class="${button_styling}">Calculate</button>
                 </div>
             `;
-        case "passing-content":
-            return `
-                    <div class="${input_form_styling}">
-                        <div id="toggle-section" class="flex items-center justify-between w-[250px]">
-                            <span id="toggle-text" class="mr-2 text-left flex-1">Displaying All Satellites</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="w-8 h-8 cursor-pointer ml-auto" id="toggle-svg">
-                                <path id="toggle-path" fill="#ffffff" d="M384 128c70.7 0 128 57.3 128 128s-57.3 128-128 128l-192 0c-70.7 0-128-57.3-128-128s57.3-128 128-128l192 0zM576 256c0-106-86-192-192-192L192 64C86 64 0 150 0 256S86 448 192 448l192 0c106 0 192-86 192-192zM192 352a96 96 0 1 0 0-192 96 96 0 1 0 0 192z"/>
-                            </svg>
-                        </div>
-                        <input type="text" id="location-2" placeholder="Location" class="${input_styling}">
-                        <button data-ripple="true" id="get-location-button-2" class="${location_button_styling}">Use Current Location</button>
-                        <button data-ripple="true" id="select-location-button" class="${location_button_styling}">Select Location</button>
-                    </div>
-                `;
         default:
             return "";
     }
