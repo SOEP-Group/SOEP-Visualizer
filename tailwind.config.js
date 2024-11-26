19f8f27e21fda5ba9d98c7ae71a835a0db2c8a1f
Limeiz <ellen.c.asman@gmail.com>
2024-11-26 14:26:00 +0100

    smaller text and boxes on mobile in tabs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./public/**/*.{html,js}", "./views/**/*.ejs"],
 
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+    },
     extend: {
       backgroundImage: {
         "loading-screen": "url('/images/background/earth-from-space.jpg')",
