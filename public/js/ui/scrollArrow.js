4224ba70c23e4cd0c2883fbfd6e2757f6f2102ec
Mohammed Shakir <mohammedshakir010528@gmail.com>
2024-12-22 16:34:06 +0100

    Scrollable tabs

---
+++ public/js/ui/scrollArrow.js
@@ -0,0 +1,44 @@
+export function initializeScrollButtons(
+  tabsContainer,
+  scrollLeftButton,
+  scrollRightButton
+) {
+  const updateScrollButtons = () => {
+    const isAtStart = tabsContainer.scrollLeft <= 0;
+    const isAtEnd =
+      Math.ceil(tabsContainer.scrollLeft + tabsContainer.clientWidth) >=
+      tabsContainer.scrollWidth;
+
+    const isScrollable = tabsContainer.scrollWidth > tabsContainer.clientWidth;
+
+    if (isScrollable) {
+      scrollLeftButton.classList.toggle("hidden", isAtStart);
+      scrollRightButton.classList.toggle("hidden", isAtEnd);
+    } else {
+      scrollLeftButton.classList.add("hidden");
+      scrollRightButton.classList.add("hidden");
+    }
+  };
+
+  scrollLeftButton.classList.add("hidden");
+
+  scrollRightButton.addEventListener("click", () => {
+    const maxScrollLeft = tabsContainer.scrollWidth - tabsContainer.clientWidth;
+    const distanceToRight = maxScrollLeft - tabsContainer.scrollLeft;
+    const scrollAmount = Math.min(200, distanceToRight);
+
+    tabsContainer.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  });
+
+  scrollLeftButton.addEventListener("click", () => {
+    const distanceToLeft = tabsContainer.scrollLeft;
+    const scrollAmount = Math.min(200, distanceToLeft);
+
+    tabsContainer.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  });
+
+  tabsContainer.addEventListener("scroll", updateScrollButtons);
+  window.addEventListener("resize", updateScrollButtons);
+
+  updateScrollButtons();
+}
